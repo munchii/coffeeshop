63601419a9b0f5735b21ac4154dc686fbbd301f3
Aaron Collver <aecollver@gmail.com>
2019-11-25 17:40:56 -0800

    Enable S3 website hosting to have a default document.

--- src/website.ts
+++ src/website.ts
@@ -1,4 +1,4 @@
-import { CfnCloudFrontOriginAccessIdentity, CloudFrontWebDistribution, SecurityPolicyProtocol, SSLMethod } from "@aws-cdk/aws-cloudfront";
+import { CfnCloudFrontOriginAccessIdentity, CloudFrontWebDistribution, SecurityPolicyProtocol, SSLMethod, OriginProtocolPolicy } from "@aws-cdk/aws-cloudfront";
 import { CanonicalUserPrincipal } from "@aws-cdk/aws-iam";
 import { Bucket } from "@aws-cdk/aws-s3";
 import { App, Construct, RemovalPolicy, Stack, StackProps } from "@aws-cdk/core";
@@ -7,25 +7,19 @@ class WebsiteStack extends Stack {
   constructor(scope: Construct, name: string, props: StackProps) {
     super(scope, name, props);
 
-    // https://github.com/aws/aws-cdk/issues/941
-    const contentAccessIdentity = new CfnCloudFrontOriginAccessIdentity(this, "ContentAccessIdentity", {
-      cloudFrontOriginAccessIdentityConfig: {
-        comment: "coffeeshop"
-      }
-    });
-
     const content = new Bucket(this, "Content", {
+      publicReadAccess: true,
       removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html"
     });
-    content.grantRead(new CanonicalUserPrincipal(contentAccessIdentity.getAtt("S3CanonicalUserId").toString()));
 
     new CloudFrontWebDistribution(this, "Website", {
       comment: process.env.npm_config_domain_name,
       originConfigs: [{
         behaviors: [{ isDefaultBehavior: true }],
-        s3OriginSource: {
-          s3BucketSource: content,
-          originAccessIdentityId: contentAccessIdentity.ref
+        customOriginSource: {
+          originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
+          domainName: content.bucketWebsiteDomainName,
         }
       }],
       aliasConfiguration: {
