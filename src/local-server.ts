8a7ebeb90624174a03832fcfd85ef7dd70c66038
Aaron Collver <aecollver@gmail.com>
2019-11-28 19:07:54 -0800

    Add local override for development.

--- src/local-server.ts
+++ src/local-server.ts
@@ -1,6 +1,23 @@
 import express from "express";
 
+function modify(req, res, next){
+  const write = res.write;
+  res.write = function (chunk) {
+    if (process.argv[2] === "--local" && res.getHeader('Content-Type').indexOf('text/html') > -1) {
+      const replacement = chunk.toString()
+        .split("https://food.munchii.com")
+        .join("http://localhost:8100")
+      write.apply(this, [Buffer.from(replacement)]);
+    } else {
+      write.apply(this, arguments);
+    }
+  };
+
+  next();
+}
+
 const http = express();
-http.use(express.static("public"))
-http.listen(8080);
-console.log("Listening on 8080...");
+http.use(modify);
+http.use(express.static("public"));
+http.listen(8000);
+console.log("Listening on 8000...");
